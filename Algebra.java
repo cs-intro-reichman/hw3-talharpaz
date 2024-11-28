9ab15731167d3d3954fcde0076a3568da259a009
talharpaz <tal.harpaz01@gmail.com>
2024-11-28 20:53:10 +0200

    algebra9

--- Algebra.java
+++ Algebra.java
@@ -90,22 +90,30 @@ public class Algebra {
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int pow = 1;  
+		int result = 1;  
 		if (n == 0) {
 			return 1;  
 		}
+		if (x == 0) {
+			return 0;  
+		} 
+		boolean isNegativeBase = (x < 0);  
+         if (isNegativeBase) x = -x;
 		for (int i = 1; i <= n; i++) {
-			pow = times(pow, x);
+			result = times(result, x);
 		}
 	
-		return (x < 0 && n % 2 != 0) ? -pow : pow;
-	
+		if (isNegativeBase && n % 2 != 0) {
+			result = -result;
+			}
+
+			return result;
 	
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int div = x1;
+		
 		int count =0;
 		if ( x1 == 0) return 0;
 		if ( x2 == 0) return -1;
